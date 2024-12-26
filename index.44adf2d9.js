3b10196f445fb6f0236b78a4744aed516cd89b79
diachkinainna <diachkinainna@gmail.com>
2024-12-26 20:08:11 +0200

    make build

---
+++ index.44adf2d9.js
@@ -0,0 +1,2 @@
+document.body.id="tree",function e(n,t){var r=document.createElement("ul");for(var a in t){var d=document.createElement("li");d.textContent=a,Object.keys(t[a]).length>0&&e(d,t[a]),r.appendChild(d)}n.appendChild(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.44adf2d9.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,32 @@
+'use strict';
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+document.body.id = 'tree';
+const tree = document.querySelector('#tree');
+function createTree(element, data) {
+    const list = document.createElement('ul');
+    for(const key in data){
+        const listElement = document.createElement('li');
+        listElement.textContent = key;
+        if (Object.keys(data[key]).length > 0) createTree(listElement, data[key]);
+        list.appendChild(listElement);
+    }
+    element.appendChild(list);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
