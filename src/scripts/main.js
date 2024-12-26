d47c7fa41e842b0f75d211835378ef95a38072c8
diachkinainna <diachkinainna@gmail.com>
2024-12-26 20:07:39 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,26 @@ const food = {
   },
 };
 
+document.body.id = 'tree';
+
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    const listElement = document.createElement('li');
+
+    listElement.textContent = key;
+
+    if (Object.keys(data[key]).length > 0) {
+      createTree(listElement, data[key]);
+    }
+
+    list.appendChild(listElement);
+  }
+
+  element.appendChild(list);
 }
 
 createTree(tree, food);
